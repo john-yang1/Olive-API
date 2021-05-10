14b5d953613f21e8cbe18426e0c87bb5a42b096c
Jeffry Lam Tang <jeffrylamtang@gmail.com>
2021-05-10 13:06:17 -0700

    Adding configuration to connect to amazon RDS, adding base requirement for postgres

--- basic/settings.py
+++ basic/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'basic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('RDS_DB_NAME'),
+        'USER': os.getenv('RDS_USERNAME'),
+        'PASSWORD': os.getenv('RDS_PASSWORD'),
+        'HOST': os.getenv('RDS_HOSTNAME'),
+        'PORT': os.getenv('RDS_PORT'),
     }
 }
 
